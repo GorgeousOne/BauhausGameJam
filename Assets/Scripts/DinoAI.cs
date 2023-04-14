e6203d595194b388bdabfcfc94aa2f9fe0814bb2
kyukado <kyukado2020@gmail.com>
2023-04-14 22:11:32 +0200

    Dino stages 1

--- Assets/Scripts/DinoAI.cs
+++ Assets/Scripts/DinoAI.cs
@@ -6,8 +6,20 @@ using UnityEngine;
 
 public class DinoAI : MonoBehaviour
 {
-
-    
+    enum InfectionStage{
+        Dino,
+        Transform,
+        Kawaii,
+        Explode
+    }
+    InfectionStage Stage = InfectionStage.Dino;
+    public int infectionLevel = 0;
+    public int Infected = 2;
+    public int KawaiiOverload = 4;
+    float Timer = 0.0f;
+    public float tranformationTime = 5.0f;
+    public float explosionTime = 5.0f;
+        
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +30,56 @@ public class DinoAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        switch(Stage) 
+        {
+            case InfectionStage.Dino:
+                if(infectionLevel >= Infected)
+                {
+                    Stage = InfectionStage.Transform;
+                    
+                }
+
+                Debug.Log("Dino");
+                break;
+
+            case InfectionStage.Transform:
+                if (Timer > tranformationTime)
+                {
+                    Stage = InfectionStage.Kawaii;
+                    Timer = 0;
+                    break;
+                }
+                Timer = Timer + Time.deltaTime;
+
+                Debug.Log("transform");
+                break;
+
+            case InfectionStage.Kawaii:
+                if(infectionLevel >= KawaiiOverload)
+                {
+                    Stage = InfectionStage.Explode;
+                }
+
+                Debug.Log("kawaii");
+                break;
+
+            case InfectionStage.Explode:
+                if (Timer > explosionTime)
+                {
+                    
+                    // EXPLODE!!
+                    Destroy(gameObject);
+                }
+                Timer = Timer + Time.deltaTime;
+                
+                Debug.Log("Explosion");
+                break;
+        }
+    }
+
+    void transformKawaii()
+    {
+
     }
 }
