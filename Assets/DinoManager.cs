e06d2db16cf42add11bc46e352222babb1f7a364
Aaron <2klaus.4peter@gmail.com>
2023-04-15 19:24:11 +0200

    WIP add dino spawn manager

---
+++ Assets/DinoManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DinoManager : MonoBehaviour {
+    //[SerializeField] private float gameWidth = 10f * 16 / 9;
+    //[SerializeField] private float gameHeight = 10;
+    [SerializeField] private Vector2 spawn = new (0, 0);
+
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    private void _spawnRandom(GameObject dinoPrefab) {
+        
+    }
+}
