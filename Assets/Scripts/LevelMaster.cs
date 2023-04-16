f5dbbd557a70ac029773794ce3541f596dd9a787
Kyukado <kyukado2020@gmail.com>
2023-04-16 09:31:00 +0200

    Update LevelMaster.cs

--- Assets/Scripts/LevelMaster.cs
+++ Assets/Scripts/LevelMaster.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class LevelMaster : MonoBehaviour
 {
+    public int Score;
+    int Level;
+	GameObject Player;
+    GameObject[] DinoCollection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		Player = GameObject.FindWithTag("Player");
+	}
 
     // Update is called once per frame
     void Update()
